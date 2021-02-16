c33b88fb718f3522d9ad7f082aa5b4691b80324e
toast-ceo <70907737+toast-ceo@users.noreply.github.com>
2021-02-16 16:57:39 +0900

    profile

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "86007497bcb93dbe7c7821572007c78c",
-"/": "86007497bcb93dbe7c7821572007c78c",
-"main.dart.js": "a4943881a954e32e1efbbdf5e6badb95",
+"index.html": "884729d92b5bf519b50750cc33ac958a",
+"/": "884729d92b5bf519b50750cc33ac958a",
+"main.dart.js": "51a78301aff52a456ee4aa73a49fca9a",
 "manifest.json": "d6948edbb908560f44c1d342b3867b6a",
 "version.json": "194954e78cde421cef0d1fed97b21675"
 };
